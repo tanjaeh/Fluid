78d78c7ba06e5aadcac6c20f5118ca7098061c59
tanjaeh <tanjaehenriksen@gmail.com>
2021-11-16 14:13:55 +0100

    basic animation added

--- FlightMovement.py
+++ FlightMovement.py
@@ -1,35 +1,43 @@
 import numpy as np
 from mayavi import mlab
 import xlrd
+import os
 
-@mlab.animate(delay = 100)
-def updateAnimation(flight):
-    line = 26
+# from Sphere import plot_sphere, test_points3d
 
-    # Exel file
-    # C:\dev\DTE3600_Fluid\
-    # loc = ("data\Sim DataTurbanalyse Innflygningslinje   17.07.20.xlsx")
-    # wb = xlrd.open_workbook(loc)
-    # sheet = wb.sheet_by_index(0)
+@mlab.animate(delay = 50)
+def updateAnimation(flight, sheet):
+    f = mlab.gcf()
+    line = 1024
 
 
     while True:
-        # x_data = sheet.cell_value(line, 0)
-        # y_data = sheet.cell_value(line, 1)
-        # z_data = sheet.cell_value(line, 2)
+        print("Line: " + str(line))
 
-        print("HELLO")
-        # print(str(x_data) + str(y_data) + str(z_data))
-        #
-        # flight.mlab_source.set(x=x_data, y=y_data, z=z_data)
-        flight.mlab_source.set(x=0, y=line, z=0)
+        x_data = sheet.cell_value(line, 0)
+        y_data = sheet.cell_value(line, 1)
+        z_data = sheet.cell_value(line, 2)
 
-        if line == 1024:
+        print(str(x_data) + " " + str(y_data) + " " + str(z_data))
+
+        # flight.mlab_source.set(x=0, y=0, z=line)
+        flight.mlab_source.set(x=x_data, y=y_data, z=z_data)
+
+        if line == 26:
             break
 
-        line += 1
+        line -= 1
         yield
 
-ball = mlab.points3d(1.0, 0.0, 0.0)
-updateAnimation(ball)
+# Exel file
+loc = (r"C:\dev\DTE3600_Fluid\data\simData.xlsx")
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+# mlab.plot3d(x, y, z, tube_radius=4, tube_sides=20)
+
+
+ball = mlab.points3d(1.0, 0.0, 0.0, scale_factor=20.0)
+
+updateAnimation(ball, sheet)
 mlab.show()
